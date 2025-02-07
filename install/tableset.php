2c850d15f9c4f3ed229295235f12f490795b4cd4
Alexander Walther <mail@alexplus.de>
2025-02-07 16:39:48 +0100

    Update tableset.php

---
+++ install/tableset.php
@@ -0,0 +1,8 @@
+<?php
+
+// Wenn YForm installiert ist, dann die YForm-Tabellen anlegen
+
+if (rex_addon::get('yform')->isAvailable()) {
+    rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/googleplaces_review.tableset.json', '[]'));
+    rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/googleplaces_place_detail.json', '[]'));    
+}
