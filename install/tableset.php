b87c81fe9005881f615df510b98b6a385e60a9b1
Alexander Walther <mail@alexplus.de>
2025-02-07 22:30:20 +0100

    Update tableset.php

--- install/tableset.php
+++ install/tableset.php
@@ -4,6 +4,6 @@
 
 if (rex_addon::get('yform')->isAvailable()) {
     rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/googleplaces_review.tableset.json', '[]'));
-    rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/googleplaces_place_detail.json', '[]'));
+    rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/googleplaces_place_detail.tableset.json', '[]'));
     rex_yform_manager_table::deleteCache();
 }
