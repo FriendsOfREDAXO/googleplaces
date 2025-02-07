366129925faa36b0003460f81447cb8c5e239e3f
Alexander Walther <mail@alexplus.de>
2025-02-07 19:53:16 +0100

    Umbau Helper.php - mehr YOrm, be_manager_relation-Feld befüllen

--- lib/Helper.php
+++ lib/Helper.php
@@ -9,7 +9,7 @@ use DateTime;
 use rex_yform_value_uuid;
 
 /**
- * Google Places AddOn: Gibt Details zu einem Google Place aus.
+ * Google Places Add-on: Gibt Details zu einem Google Place aus.
  */
 class Helper
 {
@@ -19,16 +19,16 @@ class Helper
      * @return array
      * https://developers.google.com/maps/documentation/places/web-service/details?hl=de
      */
-    public static function gapi(string $place_id = null) : array
+    public static function gapi(string $place_id = null): array
     {
 
-        if($place_id == null) {
+        if ($place_id == null) {
             $place_id = rex_addon::get('googleplaces')->getConfig('gmaps-location-id');
         }
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://maps.googleapis.com/maps/api/place/details/json?place_id='.$place_id.'&key='.rex_addon::get('googleplaces')->getConfig('gmaps-api-key').'&reviews_no_translations=true&reviews_sort=newest',
+            CURLOPT_URL => 'https://maps.googleapis.com/maps/api/place/details/json?place_id=' . $place_id . '&key=' . rex_addon::get('googleplaces')->getConfig('gmaps-api-key') . '&reviews_no_translations=true&reviews_sort=newest',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
@@ -44,55 +44,47 @@ class Helper
         return $response;
     }
 
-
-    /**
-     * Ruft Details zu einem Google Place direkt über die Google-PLaces-API ab.
-     * @param string $qry
-     * @return array | string
-     * @author Daniel Springer
-     */
-    public static function get(string $qry = "") : array
-    {
-        return self::getFromGoogle($qry);
-    } // EoF
-
     /**
      * Ruft Details zu einem Google Place direkt über die Google API ab.
-     * @param string $qry
+     * @param string $place_id
      * @return array | string
      * @author Daniel Springer
      */
-    public static function getFromGoogle(string $qry = "")
+    public static function getFromGoogle(string $place_id = null): array | string
     {
-        $response = self::gapi();
-        if ($qry == "") {
+        if ($place_id === null) {
+            $place_id = rex_addon::get('googleplaces')->getConfig('gmaps-location-id');
+        }
+        $response = self::gapi($place_id);
+        if ($place_id == "") {
             return $response;
         } else {
-            return $response[$qry];
+            return $response[$place_id];
         }
-
-    } // EoF
+    }
 
     /**
      * Ruft Details zu einem Google Place über die eigene DB ab.
      * @return array | false
      * @author Daniel Springer
      */
-    public static function getPlaceDetails($qry = "") : array | false
+    public static function getPlaceDetails($place_id = null): array | false
     {
-        $sql = rex_sql::factory();
-        $sql->setQuery('SELECT api_response_json FROM googleplaces_place_detail WHERE place_id = :place_id', ["place_id" => rex_addon::get('googleplaces')->getConfig('gmaps-location-id')]);
-        if ($sql->getRows() > 0) {
-            $response = $sql->getArray();
-            $response = rex_var::toArray($response[0]['api_response_json']);
-            if ($qry == "") {
-                return $response;
-            } else {
-                return $response[$qry];
-            }
+
+        if($place_id === null) {
+            $place_id = rex_addon::get('googleplaces')->getConfig('gmaps-location-id');
         }
-        return false;
-    } // EoF getPlaceDetails
+
+        $place = Place::query()
+            ->where('place_id', $place_id)
+            ->findOne();
+
+        if ($place) {
+            return json_decode($place->getApiResponseJson(), true);
+        } else {
+            return false;
+        }
+    }
 
     /**
      * Ruft Reviews zu einem Google Place direkt über die Google API ab (wsl. limitiert auf die letzten 5).
@@ -104,23 +96,23 @@ class Helper
         $qry = 'reviews';
         $response = self::gapi();
         return $response['result'][$qry];
-    } // EoF
+    }
 
     /**
      * Ruft alle Reviews zu einem Google Place aus der eigenen DB ab.
      * @return array
      * @author Daniel Springer
      */
-    public static function getAllReviews(string $orderBy = "", int $limit = null) : array
+    public static function getAllReviews(string $orderBy = "", int $limit = null): array
     {
         $sql = rex_sql::factory();
         $qry = 'SELECT * FROM rex_googleplaces_review';
 
         if ($orderBy != "") {
-            $qry .= ' ORDER BY '.$orderBy;
+            $qry .= ' ORDER BY ' . $orderBy;
         }
         if ($limit != "") {
-            $qry .= ' LIMIT '.$limit;
+            $qry .= ' LIMIT ' . $limit;
         }
         $sql->setQuery($qry);
 
@@ -141,14 +133,14 @@ class Helper
             $response[$id]['google_place_id'] = $row->getValue('google_place_id');
         }
         return $response;
-    } // EoF
+    }
 
     /**
      * Ruft die durschnittliche Bewertung aller Reviews zu einem Google Place aus der eigenen DB ab.
      * @return float
      * @author Daniel Springer
      */
-    public static function getAvgRating() : float
+    public static function getAvgRating(): float
     {
         $sql = rex_sql::factory();
         $sql->setQuery('SELECT rating FROM rex_googleplaces_review');
@@ -157,68 +149,35 @@ class Helper
         foreach ($sql as $row) {
             $rating = $rating + $row->getValue('rating');
         }
-        return round(floatval($rating/$i), 1);
-    } // EoF
+        return round(floatval($rating / $i), 1);
+    }
 
     /**
      * Ruft die Anzahl aller Reviews zu einem Google Place aus der eigenen DB ab.
      * @return int
      * @author Daniel Springer
      */
-    public static function getTotalRatings() : int
+    public static function getTotalRatings(): int
     {
         $sql = rex_sql::factory();
         $sql->setQuery('SELECT * FROM rex_googleplaces_review');
         $i = $sql->getRows();
         return $i;
-    } // EoF
+    }
 
     /**
      * Holt die Reviews von der Google API und speichert sie in der DB. Wenn der Eintrag bereits vorhanden ist, wird
      * er nicht verändert.
      * @return bool
-     * @author Daniel Springer
+     * @author Daniel Springer, Alexander Walther
      */
-    public static function updateReviewsDB() : bool
+    public static function updateReviewsDB(): bool
     {
         $googlePlace    = self::getFromGoogle();
         $googleReviews  = $googlePlace['reviews'];
         $googlePlaceId  = rex_addon::get('googleplaces')->getConfig('gmaps-location-id');
 
-        foreach ($googleReviews as $gr) {
-            $uuid = rex_yform_value_uuid::guidv4($gr['author_url']);
-
-            // Statt SQL-Query via rex_sql, den Eintrag über Review-Model prüfen
-            $existingReview = Review::query()
-                ->where('uuid',$uuid)
-                ->findOne();
-
-            if (!$existingReview) {
-                // Neuen Review anlegen
-                $review = Review::create();
-                
-                // Base64 Profilbild holen wenn verfügbar 
-                $gr_profile_photo_base64 = @file_get_contents($gr['profile_photo_url']); 
-                if ($gr_profile_photo_base64 !== false) {
-                    $gr_profile_photo_base64 = base64_encode($gr_profile_photo_base64);
-                }
-
-                // Review-Daten über Model-Methoden setzen
-                $review->setAuthorName($gr['author_name'])
-                    ->setAuthorUrl($gr['author_url'])
-                    ->setRating($gr['rating'])
-                    ->setText($gr['text'])
-                    ->setTime($gr['time'])
-                    ->setProfilePhotoUrl($gr['profile_photo_url'])
-                    ->setProfilePhotoBase64($gr_profile_photo_base64)
-                    ->setGooglePlaceId($googlePlaceId)
-                    ->setCreatedate((new DateTime('NOW'))->format('Y-m-d H:i:s'))
-                    ->setUuid($uuid);
-
-                // Review speichern 
-                $review->save();
-            }
-        }
+        $success = false;
 
         // Get existing place or create new one
         $place = Place::query()
@@ -232,9 +191,51 @@ class Helper
         // Update place details
         $place->setApiResponseJson(json_encode($googlePlace, \JSON_PRETTY_PRINT))
             ->setUpdatedate((new DateTime('NOW'))->format('Y-m-d H:i:s'));
-            
-        // Save place
-        return $place->save();
-    }
 
+        // Save place
+        $success = $place->save();
+
+        if ($success) {
+
+            $place_dataset_id = $place->getId();
+
+            foreach ($googleReviews as $gr) {
+                $uuid = rex_yform_value_uuid::guidv4($gr['author_url']);
+
+                // Statt SQL-Query via rex_sql, den Eintrag über Review-Model prüfen
+                $review = Review::query()
+                    ->where('uuid', $uuid)
+                    ->findOne();
+
+                if (!$review) {
+                    // Neuen Review anlegen
+                    $review = Review::create()
+                    ->setPlaceId($place_dataset_id)
+                    ->setCreatedate((new DateTime('NOW'))->format('Y-m-d H:i:s'));
+                }
+
+                // Base64 Profilbild holen wenn verfügbar
+                $gr_profile_photo_base64 = @file_get_contents($gr['profile_photo_url']);
+                if ($gr_profile_photo_base64 !== false) {
+                    $gr_profile_photo_base64 = base64_encode($gr_profile_photo_base64);
+                }
+
+                // Review-Daten über Model-Methoden setzen
+                $review->setAuthorName($gr['author_name'])
+                    ->setAuthorUrl($gr['author_url'])
+                    ->setRating($gr['rating'])
+                    ->setText($gr['text'])
+                    ->setTime($gr['time'])
+                    ->setProfilePhotoUrl($gr['profile_photo_url'])
+                    ->setProfilePhotoBase64($gr_profile_photo_base64)
+                    ->setGooglePlaceId($googlePlaceId)
+                    ->setUpdatedate((new DateTime('NOW'))->format('Y-m-d H:i:s'))
+                    ->setUuid($uuid);
+
+                // Review speichern
+                $success = ($review->save() ?: $success);
+            }
+        }
+        return $success;
+    }
 }
