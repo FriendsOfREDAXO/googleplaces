1640c4a3ba7c759f5ed2fc66b92b2efd69eb1322
Alexander Walther <mail@alexplus.de>
2025-02-07 20:01:05 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -5,21 +5,19 @@ use FriendsOfRedaxo\GooglePlaces\Review;
 
 \rex_cronjob_manager::registerType('FriendsOfRedaxo\GooglePlaces\Cronjob');
 
-
 if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
 	rex_yform_manager_dataset::setModelClass(
 		'rex_googleplaces_place_detail',
-		Place::class, // Hier anpassen, falls Namespace verwendet wird
+		Place::class
 	);
 
 	rex_yform_manager_dataset::setModelClass(
 		'rex_googleplaces_review',
-		Review::class, // Hier anpassen, falls Namespace verwendet wird
+		Review::class
 	);
 }
 
 if (in_array(rex_be_controller::getCurrentPagePart(1), ['yform', 'googleplaces'], true)) {
 	rex_extension::register('YFORM_DATA_LIST', Place::epYformDataList(...));
 	rex_extension::register('YFORM_DATA_LIST', Review::epYformDataList(...));
-
 }
