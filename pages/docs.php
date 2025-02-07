b6c5b2fac72c662ab26b206b667fcc746a286174
Alexander Walther <mail@alexplus.de>
2025-02-07 16:17:59 +0100

    Update docs.php

--- pages/docs.php
+++ pages/docs.php
@@ -1,30 +1,48 @@
 <?php
-$code = "";
-$code .= "<?php" . PHP_EOL;
-$code .= "	//Ein Array mit dem gesamten Ergebnis aus der eigenen Datenbank zurückbekommen:" . PHP_EOL;
-$code .= "	dump( gplace::getPlaceDetails() );" . PHP_EOL;
-$code .= PHP_EOL;
-$code .= "	//Den Namen des Place:" . PHP_EOL;
-$code .= "	echo gplace::getPlaceDetails('name');" . PHP_EOL;
-$code .= PHP_EOL;
-$code .= "	//Anzahl User Bewertungen:" . PHP_EOL;
-$code .= "	echo gplace::getPlaceDetails('user_rating_total');" . PHP_EOL;
-$code .= PHP_EOL;
-$code .= "	//Ein Array mit dem gesamten Ergebnis direkt über die Google-API (kostenpflichtig) zurückbekommen:" . PHP_EOL;
-$code .= "	dump( gplace::get() );" . PHP_EOL;
-$code .= PHP_EOL;
-$code .= "?>";
+
+/**
+ * @var rex_addon $this
+ * @psalm-scope-this rex_addon
+ */
+
+$mdFiles = [];
+foreach (glob(rex_path::addon('googleplaces', 'docs/*.md')) as $file) {
+    $mdFiles[mb_substr(basename($file), 0, -3)] = $file;
+}
+
+$currenMDFile = rex_request('mdfile', 'string', '01_intro');
+if (!array_key_exists($currenMDFile, $mdFiles)) {
+    $currenMDFile = '01_a_intro';
+}
+
+$page = rex_be_controller::getPageObject('googleplaces/docs');
+
+if (null !== $page) {
+    foreach ($mdFiles as $key => $mdFile) {
+        $keyWithoudPrio = mb_substr($key, 3);
+        $currenMDFileWithoudPrio = mb_substr($currenMDFile, 3);
+        $page->addSubpage(
+            (new rex_be_page($key, rex_i18n::msg('googleplaces_docs_' . $keyWithoudPrio)))
+            ->setSubPath($mdFile)
+            ->setHref('index.php?page=googleplaces/docs&mdfile=' . $key)
+            ->setIsActive($key == $currenMDFile),
+        );
+    }
+}
+
+echo rex_view::title($this->i18n('googleplaces_title'));
+
+[$Toc, $Content] = rex_markdown::factory()->parseWithToc(rex_file::require($mdFiles[$currenMDFile]), 2, 3, [
+    rex_markdown::SOFT_LINE_BREAKS => false,
+    rex_markdown::HIGHLIGHT_PHP => true,
+]);
 
 $fragment = new rex_fragment();
-$fragment->setVar('class', 'info', false);
-$fragment->setVar('title', 'Beispiel: Module Output', false); //translate
-$fragment->setVar('body', rex_string::highlight($code), false);
+$fragment->setVar('content', $Content, false);
+$fragment->setVar('toc', $Toc, false);
+$content = $fragment->parse('core/page/docs.php');
+
+$fragment = new rex_fragment();
+$fragment->setVar('title', rex_i18n::msg('package_help') . ' ', false);
+$fragment->setVar('body', $content, false);
 echo $fragment->parse('core/page/section.php');
-
-
-if (rex_addon::get('googleplaces')->getConfig('gmaps-api-key') && rex_addon::get('googleplaces')->getConfig('gmaps-location-id')) {
-    echo '<h3>Array des konfigurierten Google Place</h3>
-	<p>Die einzelnen Werte können wie oben beschrieben über <code>gplace::get(\'name_des_wertes\')</code> (kostenpflichtig über die Google-API) oder <code>gplace::getPlaceDetails(\'name_des_wertes\')</code> (gratis aus der eigenen DB) geholt werden.</p>
-	';
-    dump(gplace::getPlaceDetails());
-}
