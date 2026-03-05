ba403fd2cf35f7abb0bf46eec858f486ce779271
Tobias Krais <tk@design-to-use.de>
2026-03-05 09:33:24 +0100

    bugfix tableset reinstalling addon

--- install/tableset.php
+++ install/tableset.php
@@ -2,6 +2,13 @@
 
 // Wenn YForm installiert ist, dann die YForm-Tabellen anlegen
 if (rex_addon::get('yform')->isAvailable()) {
+    // Migration: choice_status zu choice (yform_field Abhängigkeit entfernt)
+    $sql = rex_sql::factory();
+    $sql->setQuery(
+        'DELETE FROM ' . rex::getTable('yform_field') . ' WHERE table_name = :table AND name = :name AND type_name = :type',
+        [':table' => 'rex_googleplaces_review', ':name' => 'status', ':type' => 'choice_status']
+    );
+
     rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/googleplaces_review.tableset.json', '[]'));
     rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/googleplaces_place_detail.tableset.json', '[]'));
     rex_yform_manager_table::deleteCache();
